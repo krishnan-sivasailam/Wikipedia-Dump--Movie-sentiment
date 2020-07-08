e6e4b806de340c54edb61d5c63cc8163b097a697
ksivasai <35936816+ksivasai@users.noreply.github.com>
2020-07-08 18:52:56 -0400

    Update alternate.py

--- alternate.py
+++ alternate.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 from flask import Flask, jsonify, request, render_template
 import pickle
-import simplejson
 import requests
+import ndjson
 
 with open('found_movies.ndjson') as fin:
-    movies = [simplejson.loads(l) for l in fin]
+    movies = [ndjson.loads(l) for l in fin]
 
 # Remove non-movie articles
 movies_with_wikipedia = [movie for movie in movies if 'Wikipedia:' in movie[0]]
