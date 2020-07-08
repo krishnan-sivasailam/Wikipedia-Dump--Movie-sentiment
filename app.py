9d836e145a89b2ba4ac1f45862895b98072bdcc0
ksivasai <35936816+ksivasai@users.noreply.github.com>
2020-07-08 19:48:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def masterlist(movie):
 
 # In[ ]:
 
-with open('found_movies.ndjson','b') as fin:
+with open('found_movies.ndjson','r') as fin:
     movies = [json.loads(l) for l in fin]
 
 # Remove non-movie articles
